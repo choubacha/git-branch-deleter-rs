917f069386bcef9006ee5abbebb5d020e88c0c34
Kevin Choubacha <kchoubacha@weedmaps.com>
2017-12-23 17:00:05 -0800

    Made prompts colored

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
 extern crate git2;
+extern crate colored;
 use git2::{Repository, BranchType};
 use std::path::Path;
 use std::io;
 use std::io::Write;
+use colored::*;
 
 fn main() {
     let repo = Repository::open(&Path::new(".")).unwrap();
     let branches = repo.branches(Some(BranchType::Local)).unwrap();
     for mut m in branches {
         if let Ok((ref mut branch, BranchType::Local)) = m {
+            let branch_name = branch.name().unwrap().unwrap().yellow();
             if branch.is_head() {
-                println!("Skipping branch {} as it is currently HEAD", branch.name().unwrap().unwrap());
+                println!("Skipping branch {} as it is currently {}",
+                         branch_name,
+                         "HEAD".green());
                 continue;
             }
 
             'branch_questions: loop {
-                print!("\nbranch: {}\nDelete? (y/n): ", branch.name().unwrap().unwrap());
+                print!("\nbranch: {}\nDelete? (y/n): ", branch_name);
                 io::stdout().flush().unwrap();
 
                 let mut buffer = String::new();
@@ -23,16 +28,16 @@ fn main() {
                 match buffer.trim().to_lowercase().as_str() {
                     "yes" | "y" => {
                         match branch.delete() {
-                            Ok(_) => println!("Deleted"),
+                            Ok(_) => println!("{}", "Deleted".red()),
                             Err(e) => println!("Error deleting: {}", e),
                         };
                         break 'branch_questions;
                     },
                     "no" | "n" | "" => {
-                        println!("Skipped");
+                        println!("{}", "Skipped".yellow());
                         break 'branch_questions;
                     },
-                    other => println!("Did not recognize {}", other),
+                    other => println!("Did not recognize {}", other.yellow()),
                 }
             }
         }
